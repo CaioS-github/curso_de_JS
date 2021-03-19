a990a9bd14824eb86d165e3f7b693cd8676ee912
CaioS-github <77083015+CaioS-github@users.noreply.github.com>
2021-03-19 15:55:10 -0300

    19/03/2021

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 100
+console.log('A velocidade do seu carro é ${velocidade}km/h')
+if (velocidade > 90){
+    console.log('Você ultrapassou a velocidade permitida. Você foi MULTADO!')
+}
+console.log('Dirija com cuidado!')
